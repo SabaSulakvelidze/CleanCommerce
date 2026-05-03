0471d5d0c63ced5d6b6b9accac520544fe5c3de2
Saba <sabasulakveidze@gmail.com>
2026-05-03 18:16:49 +0400

    feat: added Enities and enums

---
+++ CleanCommerce.Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanCommerce.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; }
+
+        public ICollection<UserRole> UserRoles { get; set; } = [];
+        public ICollection<CartItem> CartItems{ get; set; } = [];
+        public ICollection<Order> Orders{ get; set; } = [];
+
+    }
+}
