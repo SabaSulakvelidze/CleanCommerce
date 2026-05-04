afc7f369ebefabba329e0f8b27540fef62f011ea
Saba <sabasulakveidze@gmail.com>
2026-05-04 21:43:44 +0400

    feat: added program file and dependency injection

---
+++ CleanCommerce.Infrastructure/DependencyInjection/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using CleanCommerce.Infrastructure.Presistance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanCommerce.Infrastructure.DependencyInjection
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(option => option.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            return services;
+        }
+    }
+}
