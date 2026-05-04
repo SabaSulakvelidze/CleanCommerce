90493c21e7a3a29902ccdf653ee93559a7ce3c16
Saba <sabasulakveidze@gmail.com>
2026-05-04 23:39:44 +0400

    feat removed factory, updated swashbuckle

--- CleanCommerce.Infrastructure/Migrations/20260504193807_InitialCreate.Designer.cs
+++ CleanCommerce.Infrastructure/Migrations/20260504193807_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanCommerce.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260504180042_InitialCreate")]
+    [Migration("20260504193807_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
