afe6415451f5c64a8e56e3f67495b06a608411c6
Saba <sabasulakveidze@gmail.com>
2026-05-05 21:43:23 +0400

    feat: added Interfaces

---
+++ CleanCommerce.Application/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using CleanCommerce.Domain.Entities;
+
+namespace CleanCommerce.Application.Interfaces.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetAllAsync();
+        Task<Category?> GetByIdAsync(int id);
+        Task<Category?> GetByNameAsync(string name);
+        Task<Category> AddAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task DeleteAsync(Category category);
+        
+    }
+}
---
+++ CleanCommerce.Application/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿
+
+using CleanCommerce.Domain.Entities;
+
+namespace CleanCommerce.Application.Interfaces.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetAllAsync();
+        Task<Product?> GetByIdAsync(int id);
+        Task<List<Product>> GetByCategoryIdAsync(int categoryId);
+        Task<Product> AddAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task DeleteAsync(Product product);
+    }
+}
