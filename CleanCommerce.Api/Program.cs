876daf0d5965a6a362a1d9651c455346bdd7f35b
Saba <sabasulakveidze@gmail.com>
2026-05-06 12:56:18 +0400

    feat: DI config done

--- CleanCommerce.Api/Program.cs
+++ CleanCommerce.Api/Program.cs
@@ -1,3 +1,4 @@
+using CleanCommerce.Application.DependencyInjection;
 using CleanCommerce.Infrastructure.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
