0a779df06f67b72c860eecd760314424d54731fc
Saba <sabasulakveidze@gmail.com>
2026-05-08 16:23:31 +0400

    feat: added cart dtos

---
+++ CleanCommerce.Application/DTOs/Requests/AddToCartRequest.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace CleanCommerce.Application.DTOs.Requests
+{
+    public class AddToCartRequest
+    {
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int ProductId { get; set; }
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
---
+++ CleanCommerce.Application/DTOs/Responses/CartItemResponse.cs
@@ -0,0 +1,15 @@
+﻿
+using CleanCommerce.Domain.Entities;
+
+namespace CleanCommerce.Application.DTOs.Responses
+{
+    public class CartItemResponse
+    {
+        public int CartItemId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal LineTotal { get; set; }
+    }
+}
