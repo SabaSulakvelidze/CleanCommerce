0e626e818ec8e185fdbba8734ebece70310de38d
Saba <sabasulakveidze@gmail.com>
2026-05-09 19:04:57 +0400

    feat: created mapping

---
+++ CleanCommerce.Application/Mapping/CartMapping.cs
@@ -0,0 +1,26 @@
+﻿
+using AutoMapper;
+using CleanCommerce.Application.DTOs.Requests;
+using CleanCommerce.Application.DTOs.Responses;
+using CleanCommerce.Domain.Entities;
+
+namespace CleanCommerce.Application.Mapping
+{
+    public class CartMapping : Profile
+    {
+        public CartMapping()
+        {
+            CreateMap<AddToCartRequest, CartItem>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.Product, opt => opt.Ignore())
+                .ForMember(dest => dest.User, opt => opt.Ignore());
+
+            CreateMap<CartItem, CartItemResponse>()
+                .ForMember(dest => dest.CartItemId, opt => opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.Name))
+                .ForMember(dest => dest.UnitPrice, opt => opt.MapFrom(src => src.Product.Price))
+                .ForMember(dest => dest.LineTotal, opt => opt.MapFrom(src => src.Product.Price*src.Quantity));
+        }
+    }
+}
