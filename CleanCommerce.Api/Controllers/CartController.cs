3a7c3b3740c403d05578b8d9ca62d7e67913ecf6
Saba <sabasulakveidze@gmail.com>
2026-05-09 19:13:18 +0400

    feat: created cart controller

---
+++ CleanCommerce.Api/Controllers/CartController.cs
@@ -0,0 +1,48 @@
+﻿using CleanCommerce.Application.DTOs.Requests;
+using CleanCommerce.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanCommerce.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController(ICartService cartService) : ControllerBase
+    {
+        [HttpGet("me")]
+        [Authorize]
+        public async Task<IActionResult> GetMyCart()
+        {
+            return Ok(await cartService.GetMyCartAsync());
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> AddToCart(AddToCartRequest request)
+        {
+            return Ok(await cartService.AddToCartAsync(request));
+        }
+
+        [HttpPut("{cartItemId}")]
+        [Authorize]
+        public async Task<IActionResult> UpdateCartItem(int cartItemId, UpdateCartItemRequest request)
+        {
+            return Ok(await cartService.UpdateCartItemASync(cartItemId, request));
+        }
+
+        [HttpDelete("{cartItemId}")]
+        [Authorize]
+        public async Task<IActionResult> RemoveCartItem(int cartItemId)
+        {
+            return Ok(await cartService.RemoveCartItemAsync(cartItemId));
+        }
+
+        [HttpDelete("clear")]
+        [Authorize]
+        public async Task<IActionResult> ClearMyCart()
+        {
+            return Ok(await cartService.ClearMyCartAsync());
+        }
+    }
+}
